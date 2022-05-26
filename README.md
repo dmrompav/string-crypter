ef842f01ea5b0e4d00e287b7714afb71b47451ab
dmrompav <dm.rom.pav@gmail.com>
2022-05-26 19:18:15 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# <span>string-crypter</span>
+
+Methods to encrypt your text or to return decrypted.
+
+## Install
+
+```
+  npm install string-crypter
+```
+
+## Usage
+```javascript
+import { encrypt, decrypt } from 'string-crypter';
+
+const key = 'Phrase I will never forget';
+const text = 'Hello string-crypter!';
+
+const encrypted = encrypt(key)(text);
+console.log(encrypted);
+// "wmdmh0TCSfws3DfdlywS="
+
+const decrypted = decrypt(key)(encrypted);
+console.log(decrypted);
+// "Hello string-crypter!"
+```
+
+## Methods
+- **encrypt**( *key*, *text* ) // @return { String }
+- **decrypt**( *key*, *string* ) // @return { String }
+
+> **`KEY`** length shouldn't be more than `TEXT` string. Key's extra length is useless, but that is not crytical. If you are using KEY of just one symbol - it will be the "Cesar cipher".
+
+## Text/key symbols that are supported
+**EN**  
+Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz  
+**RU**  
+Аа Бб Вв Гг Дд Ее Ёё Жж Зз Ии Йй Кк Лл Мм Нн Оо Пп Рр Сс Тт Уу Фф Хх Цц Чч Шш Щщ Ъъ Ыы Ьь Ээ  Юю Яя  
+**Others**  
+1234567890 !?@#$%^&*()-=+_|/'"[]{}:;,.`~№<>
---
+++ index.js
@@ -0,0 +1,73 @@
+const map = require('./map.json');
+
+function mapCheck (letter) {
+  let value;
+  let container;
+
+  for (let [, str] of Object.entries(map)) {
+    value = str.indexOf(letter);
+    if (value >= 0) {
+      container = str;
+      break;
+    }; 
+  }
+
+  return {
+    value: value || 0,
+    str: container
+  }
+}
+
+function getRewrittenLetter(keyLetter, textLetter, target) {
+  const keyValue = mapCheck(keyLetter).value;
+  const {value, str} = mapCheck(textLetter);
+  
+  if (!str) {
+    return textLetter;
+  }
+  
+  let newValue = value + (target * (keyValue + 1));
+  if (newValue < 0) {
+    do {
+      newValue += str.length;
+    } while (newValue < 0);
+  }
+  const rewrittenValue = newValue < str.length ? newValue : newValue % str.length;
+  
+  return str[rewrittenValue];
+};
+
+function getRewrittenLetters (key, text, target) {
+  if (text && text.length && key && key.length) {
+    let encrypted = '';
+  
+    for (let i = 0; i <= text.length - 1; i++) {
+      const keyLetter = i < key.length ? key[i] : key[i % key.length];
+      const textLetter = text[i];
+      const rewrittenLetter = getRewrittenLetter(keyLetter, textLetter, target);
+  
+      encrypted += rewrittenLetter;
+    }
+  
+    return encrypted;
+  }
+
+  return text;
+};
+
+function encrypt (key) {
+  return function (text) {
+    return getRewrittenLetters(key, text, 1)
+  };
+};
+
+function decrypt (key) {
+  return function (string) {
+    return getRewrittenLetters(key, string, -1)
+  }
+};
+
+module.exports = {
+  encrypt,
+  decrypt
+};
